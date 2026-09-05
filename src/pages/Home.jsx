2a80f17e358b0baf34898db8b7303579e0fa7ded
vaishnavi90175 <2400090175@kluniversity.in>
2026-09-05 08:48:56 +0530

    completed registration and login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <h1>Welcome to User Portal</h1>
       <p className="intro">Create an account or sign in to continue.</p>
       <div className="home-actions">
-        <Link className="button button-primary" to="/registation">Create account</Link>
+        <Link className="button button-primary" to="/regisrtation">Create account</Link>
         <Link className="button button-secondary" to="/login">Sign in</Link>
       </div>
     </section>
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -10,14 +10,14 @@ function NavBar() {
         <NavLink className="brand" to="/">User Portal</NavLink>
         <nav aria-label="Main navigation">
           <NavLink to="/">Home</NavLink>
-          <NavLink to="/registation">Register</NavLink>
+          <NavLink to="/registration">Register</NavLink>
           <NavLink to="/login">Login</NavLink>
         </nav>
       </header>
       <main className="page-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/registation" element={<UserRegistration />} />
+          <Route path="/registration" element={<UserRegistration />} />
           <Route path="/login" element={<UserLogin />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
